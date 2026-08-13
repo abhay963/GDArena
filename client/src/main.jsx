e2d3c6495d64552c6d51c48835838799deae8baa
abhay963 <abhayyadav96312@gmail.com>
2026-08-13 12:31:12 +0530

    feat: add ToastContainer for notifications in main application

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,23 +1,22 @@
-// Import React
 import React from "react";
-
-// Import ReactDOM
 import ReactDOM from "react-dom/client";
-
-// Import BrowserRouter
 import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-// Import global styles
 import "./index.css";
-
-// Import App component
 import App from "./App";
 
-// Render React application
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        newestOnTop
+        theme="dark"
+      />
     </BrowserRouter>
   </React.StrictMode>
 );
